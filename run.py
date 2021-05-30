90e33b50579ac2bcb294e5f1a69f9f2f91b5d858
FestusMutie <mutiefestus84@gmail.com>
2021-05-30 23:32:30 +0300

    creating a while loop to confirm user inputs

--- run.py
+++ run.py
@@ -3,4 +3,11 @@ from user import User
 
 
 def main():
-   
+    while True: 
+        print("You have successfully joined password locker")  
+        print('\n')
+        print("select an abbreviation to navigate the program:for a new user pick 'nu':To login into your account 'lgn' or 'ext' to exit")
+        abbreviated_word = input().lower() 
+        print('\n')
+
+        
