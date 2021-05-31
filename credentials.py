2a33f06c7c52bf53a8404ab8e02e0335054f13de
FestusMutie <mutiefestus84@gmail.com>
2021-05-31 11:42:53 +0300

    add method for saving,displaying and deleting credentials

--- credentials.py
+++ credentials.py
@@ -11,4 +11,38 @@ class Credentials:
         Method that saves credentials objects into credentials_list
         '''
         self.credentials_list.append(self)
-    def delete_credential(self): 
+    def delete_credential(self):
+                '''
+        Method that deletes a particular credential
+        '''
+    Credentials.credentials_list.remove(self)
+    @classmethod
+    def find_by_name(cls, account_name):
+        '''
+        Method that takes in a name and returns a credential that matches that particular name
+        Args:
+            name: account_name that has a password
+        Return:
+            the account_name and it's corresponding Password
+        '''
+        for credential in cls.credentials_list:
+            if credential.account_name == account_name:
+                return credential
+    @classmethod
+    def credential_exists(cls, name):
+        '''
+        Method to check if the credential exists
+        Args:
+        name: name of account to search if it exists
+        boolean: True or False depending if the contatc exists
+        '''
+        for credential in cls.credentials_list:
+            if credential.account_name == name:
+                return True
+        return False
+    @classmethod
+    def display_credentials(cls):
+        '''
+        Method which displays the current credentials
+        '''
+        return cls.credentials_list
